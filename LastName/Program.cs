de30bcac7562052b7c1a4fb7d967b46135db47f4
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-13 22:14:25 +0530

    Added::LastNameCaps

---
+++ LastName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LastName
+{
+    public static class Patterns
+    {
+        public static string LastName = @"^[A-Z][a-z]{3,9}$";
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Last Name");
+            Regex r1 = new Regex(Patterns.LastName);
+            Console.WriteLine(r1.IsMatch(Console.ReadLine()));
+        }
+    }
+}
