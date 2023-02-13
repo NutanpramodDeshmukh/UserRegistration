f6f0a2d378cda65d008c7d000063927afe30e793
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-13 21:29:32 +0530

    Added:FirstNameCaps

---
+++ FirstName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace FirstName
+{
+    public static class Patterns
+    {
+        public static string FirstName = @"^[A-Z][a-z]{3,9}$";
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name");
+            Regex r1 = new Regex(Patterns.FirstName);
+            Console.WriteLine(r1.IsMatch(Console.ReadLine()));
+        }
+    }
+}
