23de7c5c1fa8567c6fba2d52e3544822579c2bb4
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-13 22:40:06 +0530

    Added::EmailValidation

---
+++ EmailValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailValidation
+{
+    public static class Patterns
+    {
+        public static string EmailPattern = @"[a-z]{3,7}.?[a-z0-9]{3,8}@[a-z]{3,8}.(com|in)$";
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Email Address");
+            Regex r1 = new Regex(Patterns.EmailPattern);
+            Console.WriteLine(r1.IsMatch(Console.ReadLine()));
+        }
+    }
+}
