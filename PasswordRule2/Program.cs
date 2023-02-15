88c691d3c3d1b83d1b90cb3d65c55b7f58ad26bf
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-15 12:10:23 +0530

    Added:Password enter atleast 8 character and 1 upper case

---
+++ PasswordRule2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordRule2
+{
+    public static class Patterns
+    {
+        public static string UserPassword = @"^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$";
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter user valid Password");
+            Regex r1 = new Regex(Patterns.UserPassword);
+            //Console.WriteLine(r1.IsMatch(Console.ReadLine()));
+            if (r1.IsMatch(Console.ReadLine()))
+            {
+                Console.WriteLine("Welcome");
+            }
+        }
+    }
+}
