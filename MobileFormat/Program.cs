d4582e5b63821f66dfc203b50e69111092feb3ac
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-15 12:01:42 +0530

    Added:Phone Number

---
+++ MobileFormat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MobileFormat
+{
+    public static class Patterns
+    {
+        public static string PhoneNumberPattern = @"(^[0-9]{10}$)|(^[0-9]{3}-[0-9]{4}-[0-9]{4}$)";
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Phone Number");
+            Regex r1 = new Regex(Patterns.PhoneNumberPattern);
+            Console.WriteLine(r1.IsMatch(Console.ReadLine()));
+        }
+    }
+}
